5d9222d56a816745310805ffcc68c3bb28753443
Olakunle Dacosta <dacosta00736@gmail.com>
2025-12-24 13:11:10 +0100

    Initial commit

---
+++ svelte.config.js
@@ -0,0 +1,19 @@
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter({
+			// Enables the experimental Bun runtime support
+            runtime: 'experimental_bun1.x'
+		})
+	}
+};
+
+export default config;
